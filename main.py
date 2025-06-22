41fc3679ca0db8dec74ca74e3e8eddcf8e8210db
SriHasnika <srihasnika@gmail.com>
2025-06-22 11:43:45 +0530

    fix: issues

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.set_page_config(page_title="Book Collection Dashboard", layout="wide")
 
-book_entry_page = st.Page("sample/pages/book_entry.py", title="Book Entry")
-book_summary_page = st.Page("sample/pages/book_summary.py", title="Book Summary")
+book_entry_page = st.Page("pages/book_entry.py", title="Book Entry")
+book_summary_page = st.Page("pages/book_summary.py", title="Book Summary")
 
 pg = st.navigation([
     book_entry_page,
